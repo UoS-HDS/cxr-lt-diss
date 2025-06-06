028c6d194eab3c34f4d9adf1a666b3b702343975
Nasir Abdulrasheed <mypythtesting@gmail.com>
2025-06-06 12:24:53 +0100

    chore: switch to default credentials download script

--- download_mimic_cxr_gc.py
+++ download_mimic_cxr_gc.py
@@ -7,15 +7,15 @@ from google.cloud import storage
 from google.oauth2 import service_account
 
 
-def download_data(bucket_name: str, data_dir: Path, key_path: str):
+def download_data(bucket_name: str, data_dir: Path):
     """download data from gc bucket"""
 
-    credentials = service_account.Credentials.from_service_account_file(key_path)
-    client = storage.Client(credentials=credentials)
+    client = storage.Client()
     bucket = client.bucket(bucket_name)
     blobs = client.list_blobs(bucket)
     for blob in blobs:
         print(blob.name)
+        return
         # Create local path
         local_path = data_dir / blob.name
         local_path.mkdir(parents=True, exist_ok=True)
@@ -26,17 +26,17 @@ def download_data(bucket_name: str, data_dir: Path, key_path: str):
 
 if __name__ == "__main__":
     BUCKET_NAME = "mimic-cxr-jpg-2.1.0.physionet.org"
-    KEY_PATH = os.getenv("GC_SERVICE_KEY_FILE")
-    if not KEY_PATH:
-        print(
-            "Set the GC_SERVICE_KEY_FILE env variable to the path "
-            "of service account json credential file"
-        )
-        os._exit(1)
-    if not Path(KEY_PATH).exists():
-        print(f"{KEY_PATH} Key file not found")
-        os._exit(1)
+    #KEY_PATH = os.getenv("GC_SERVICE_KEY_FILE")
+    #if not KEY_PATH:
+    #    print(
+    #        "Set the GC_SERVICE_KEY_FILE env variable to the path "
+    #        "of service account json credential file"
+    #    )
+    #    os._exit(1)
+    #if not Path(KEY_PATH).exists():
+    #    print(f"{KEY_PATH} Key file not found")
+    #    os._exit(1)
 
-    DATA_DIR = Path("mimic-cxr-jpg-2.1.0")
+    DATA_DIR = Path("data/mimic-cxr-jpg-2.1.0")
     DATA_DIR.mkdir(parents=True, exist_ok=True)
-    download_data(BUCKET_NAME, DATA_DIR, KEY_PATH)
+    download_data(BUCKET_NAME, DATA_DIR)
