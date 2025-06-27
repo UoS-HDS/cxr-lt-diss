6584f1f28352948381565f1fd96ca7cae093fec0
Nasir Abdulrasheed <mypythtesting@gmail.com>
2025-06-27 12:02:04 +0100

    feat: complete lightningCLI init script

--- main.py
+++ main.py
@@ -1,10 +1,42 @@
-from pathlib import Path
+import os
+
+from dotenv import load_dotenv
+import torch
+from lightning.pytorch.cli import LightningCLI
+from lightning.pytorch.loggers import NeptuneLogger
 
 
-def main():
-    print("Hello from dissertation!")
-    print((Path(__file__).parent / ".venv").exists())
+from src.models.chexfusion import CxrModel, CxrModelFusion
+from src.datasets.datamodule import CxrDataModule
+
+
+load_dotenv()
+
+
+def get_model_class():
+    """returns model class based on STAGE env variable"""
+    stage = os.getenv("STAGE", "1")
+    if stage == "1":
+        return CxrModel
+    elif stage == "2":
+        return CxrModelFusion
+    else:
+        raise ValueError(f"Unknown STAGE: {stage}. Expected '1' or '2'.")
+
+
+class CxrLightningCLI(LightningCLI):
+    def before_fit(self):
+        if isinstance(self.trainer.logger, NeptuneLogger):
+            self.trainer.logger.experiment["train/config"].upload("config.yaml")
+
+
+def cli_main():
+    torch.set_float32_matmul_precision("high")
+    print(f"Using {torch.cuda.device_count()} gpus")
+
+    model_class = get_model_class()
+    cli = CxrLightningCLI(model_class=model_class, datamodule_class=CxrDataModule)  # noqa
 
 
 if __name__ == "__main__":
-    main()
+    cli_main()
